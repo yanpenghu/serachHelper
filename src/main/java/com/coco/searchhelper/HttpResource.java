e9a2de4761e64506dddfe0ef888620782fb38eb1
胡艳棚 <xyhuyp@163.com>
2016-11-23 13:47:20 +0800

    url

--- src/main/java/com/coco/searchhelper/HttpResource.java
+++ src/main/java/com/coco/searchhelper/HttpResource.java
@@ -2,8 +2,6 @@ package com.coco.searchhelper;
 
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,23 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpResource {
 
-	public static String DOMAIN = "DOMAIN";
 	
 	@RequestMapping("search")
-	public String getIt(HttpServletRequest request,String url) throws Exception {
-		if (url.startsWith("http")) {
-			request.getSession(true).setAttribute(DOMAIN, url);
-		}
-		Response response = Request.Get(url).execute();
+	public String getIt(String q,String start) throws Exception {
+		String urlStr="https://www.googleapis.com/customsearch/v1element?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=filtered_cse&num=10&hl=zh_CN&prettyPrint=false&source=gcsc&gss=.com&sig=432dd570d1a386253361f581254f9ca1&start="+start+"&cx=002888546953948157320:bahxnr-mq7i&q="+q+"&sort=&googlehost=www.google.com";
+		Response response = Request.Get(urlStr).execute();
 		return response.returnContent().asString(StandardCharsets.UTF_8);
 	}
-
-	@RequestMapping("/")
-	public String getOther(HttpServletRequest request,String url) throws Exception {
-		if (!url.startsWith("http")) {
-			url = request.getSession(true).getAttribute(DOMAIN) + url;
-		}
-		Response response = Request.Get(url).execute();
-		return response.returnContent().asString(StandardCharsets.UTF_8);
-	}
+	
 }
