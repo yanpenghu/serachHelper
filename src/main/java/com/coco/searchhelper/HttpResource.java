2ab91a414209525d570d4f296c08b2f37cf8bead
胡艳棚 <xyhuyp@163.com>
2016-11-23 13:13:12 +0800

    test

--- src/main/java/com/coco/searchhelper/HttpResource.java
+++ src/main/java/com/coco/searchhelper/HttpResource.java
@@ -16,10 +16,8 @@ public class HttpResource {
 
 	@GET
 	@Produces(MediaType.WILDCARD)
-	public String getIt(@QueryParam("q") String q, @QueryParam("start") String start) throws Exception {
-		String urlStr = "https://www.googleapis.com/customsearch/v1element?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=filtered_cse&num=10&hl=zh_CN&prettyPrint=false&source=gcsc&gss=.com&sig=432dd570d1a386253361f581254f9ca1&start="
-				+ start + "&cx=002888546953948157320:bahxnr-mq7i&q=" + q + "&sort=&googlehost=www.google.com";
-		Response response = Request.Get(urlStr).execute();
+	public String getIt(@QueryParam("url") String url) throws Exception {
+		Response response = Request.Get(url).execute();
 		return response.returnContent().asString(StandardCharsets.UTF_8);
 	}
 
